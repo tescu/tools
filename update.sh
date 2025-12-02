578bcae11f80dc4f939a44b05c4a1d1f169052f3
tescu <tescu@tutamail.com>
2025-12-02 13:05:24 +0200

    improve update script

--- update.sh
+++ update.sh
@@ -1,16 +1,41 @@
 #!/usr/bin/env bash
 shopt -s globstar
 
-# TODO: This but reverse for installation. Also turn it into a function
-cp -v ~/.local/bin/* ./bin/
-cp -v ~/.local/etc/* ./etc/
+updatecfg() {
+	cp -v ~/.local/bin/* ./bin/
+	cp -v ~/.local/etc/* ./etc/ 
+	
+	cp -rv ~/.config/sway     ./config/
+	cp -rv ~/.config/foot     ./config/
+	cp -rv ~/.config/lf       ./config/
+	cp -rv ~/.config/fnott    ./config/
+	cp -rv ~/.config/i3blocks ./config/
+	cp -v ~/.config/user-dirs.dirs ./config/
 
-cp -rv ~/.config/sway ./config/
-cp -rv ~/.config/foot ./config/
-cp -rv ~/.config/lf ./config/
-cp -rv ~/.config/fnott ./config/
-cp -rv ~/.config/i3blocks ./config/
-#cp -v ~/.config/user-dirs.dirs ./config/
+	cp -v ~/.bashrc ./bashrc
+	cp -v ~/.bash_profile ./bash_profile
 
-cp -v ~/.bashrc ./bashrc
-cp -v ~/.bash_profile ./bash_profile
+	printf "%s\n" "Done!"
+}
+
+installcfg() {
+	read -rp "WARNING! This may overwrite important files! Continue? (y/n)"
+	cp -rv ./bin ~/.local/bin
+	cp -rv ./etc ~/.local/etc 
+	
+	cp -rvi ~/.config/* ./config/
+
+	cp -vi ./bashrc ~/.bashrc
+	cp -v ./bash_profile ~/.bash_profile
+	printf "%s\n" "Done!"
+}
+
+case "$1" in
+	# install
+	[iI]*)
+		installcfg
+	;;
+	*)
+		updatecfg
+	;;
+esac
